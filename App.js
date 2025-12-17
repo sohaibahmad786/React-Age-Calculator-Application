0b03ebcec96fb2cf6578e3c3ff7800f15a48a0bd
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2025-12-17 08:28:22 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Age_counter from './Age_counter/Age_counter'
+
+const App = () => {
+  return (
+    <div>
+      <Age_counter />
+    </div>
+  )
+}
+
+export default App
